662f6aee6191c385f5aa27f1446c38cbf1706ed8
ivk <ivan.keynes@bbconsult.co.uk>
2023-07-02 15:03:29 +0200

    Program is filled in

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SubtitleHandler;
+
+try
+{
+    string filePath = "napisy do filmu.srt";
+    TimeSpan additionalTime = new TimeSpan(0, 0, 0, 5, 880);
+    string[] file = File.ReadAllLines(filePath);
+    SubtitleProcessor subtitleProcessor = new SubtitleProcessor();
+    List<SubtitleModel> subtitleItems = subtitleProcessor.DivideSubtitles(file);
+    List<SubtitleModel> itemsWithAddedTimes = subtitleItems.AddTime(additionalTime);
+    List<SubtitleModel> itemsWithSameTimes = subtitleItems.FindSameSeconds();
+
+    using (TextWriter textWriter = new StreamWriter("napisy z poprawionym czasem.srt"))
+    {
+        for (int i = 0; i < itemsWithAddedTimes.Count; i++)
+        {
+            SubtitleModel item = itemsWithAddedTimes[i];
+            textWriter.WriteLine(string.Format("{0}", i + 1));
+            textWriter.WriteLine($"{item.StartTime} --> {item.EndTime}");
+            foreach (var text in item.Text)
+            {
+                textWriter.WriteLine($"{text}");
+            };
+            textWriter.WriteLine("");
+        }
+    }
+
+    using (TextWriter textWriter = new StreamWriter("napisy z tym samym czasem.srt"))
+    {
+        for (int i = 0; i < itemsWithSameTimes.Count; i++)
+        {
+            SubtitleModel item = itemsWithSameTimes[i];
+            textWriter.WriteLine(string.Format("{0}", i + 1));
+            textWriter.WriteLine($"{item.StartTime} --> {item.EndTime}");
+            foreach (var text in item.Text)
+            {
+                textWriter.WriteLine($"{text}");
+            };
+            textWriter.WriteLine("");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Exception: " + ex.Message);
+}
